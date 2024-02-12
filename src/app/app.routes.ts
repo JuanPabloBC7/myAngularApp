4f7c687ef49659798df77b422fc47d47b9a454d1
JuanPabloBC7 <juanpablobc7@gmail.com>
2024-02-12 17:40:24 -0600

    Creación de landing page y ruteo inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './modules/landing/landing.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { AuthComponent } from './modules/auth/auth.component';
+import { ErrorComponent } from './modules/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingComponent
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    },
+    {
+        path: '**',
+        component: ErrorComponent
+    },
+];
